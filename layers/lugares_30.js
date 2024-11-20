7e858f680e243e4a9e3c3c04d74592cbc7d03464
mapajunin <rudychavezcunyas@gmail.com>
2024-11-20 01:32:10 -0500

    Add files via upload

---
+++ layers/lugares_30.js
@@ -0,0 +1,1 @@
+var json_lugares_30 = {"type":"FeatureCollection","name":"lugares_30","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ layers/rutas_50.js
@@ -0,0 +1,1 @@
+var json_rutas_50 = {"type":"FeatureCollection","name":"rutas_50","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
